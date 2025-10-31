2622d56b5c9d0e7e8acd77d5ae88ab1cda094439
gukin-han <gukin.dev@gmail.com>
2025-10-31 21:10:12 +0900

    first commit

---
+++ src/test/java/com/example/commerceapi/CommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
